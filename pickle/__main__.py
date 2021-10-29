4d59d2997a1f080fff03e587cf8def9b04309001
Kevin <kevin@e-itheta.com>
2021-10-29 23:57:37 +0000

    add entity

---
+++ pickle/__main__.py
@@ -0,0 +1,37 @@
+import asyncio
+import os
+from . import render
+from . import entity
+
+def main():
+    
+    async def _main():
+        with open("maps/mst_campus.txt", "r") as fp:
+            mapdata = render.Map(fp)
+        player = entity.Entity("@", "Player")
+        player.position[:] = 2, 2
+        camera = render.Camera(player, mapdata)
+        camera.position[:] = 0, 0
+
+        for view in camera:
+
+            game_map = next(view)
+            player_pos = camera.relative_entity_position()
+            
+            
+            render.apply_occlusion_layer(game_map, player_pos)
+            player.render(camera)
+
+            print("\n".join("".join(r for r in row) for row in game_map), end="")
+            await asyncio.sleep(1/30)
+    
+    render.Map.lines, render.Map.columns = os.get_terminal_size()
+    
+    loop = asyncio.get_event_loop()
+    loop.create_task(_main())
+    loop.run_forever()
+
+
+# a bit redundant but whatever
+if __name__ == "__main__":
+    main()
---
+++ pickle/entity.py
@@ -0,0 +1,58 @@
+from typing import *
+
+if TYPE_CHECKING:
+    from pickle import render
+
+
+_id = 0
+def _next_id():
+    global _id
+    yield _id
+    _id += 1
+
+nextid = _next_id()
+
+
+class BaseEntity:
+
+    def __init__(self, reprchar, name):
+        self.reprchar = reprchar
+        self.name = name
+        self.position = [0.0, 0.0]
+        self.id = next(nextid)
+     
+    def __repr__(self):
+        return f"<{type(self).__name__} : {self.id}>('{self.reprchar}', '{self.name}')"
+
+
+    @property
+    def row(self):
+        return int(self.position[0])
+    
+    @property
+    def col(self):
+        return int(self.position[1])
+
+
+    def render(self, camera: "render.Camera"):
+        view = camera.current_view
+        pos = camera.relative_position(self.position)
+        if pos is None:
+            return
+            
+        # TODO copy into camera
+
+
+class Entity(BaseEntity):
+
+    def __init__(self, reprchar, name):
+        super().__init__(reprchar, name)
+        self.throttle = [0, 0]
+
+
+class Player(Entity):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        
